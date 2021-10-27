8df3c4218f4d462cd53ef6f5d7ea4bab142c89a5
Đạt Nguyễn <nguyenbatiendat@gmail.com>
2021-10-27 15:23:17 +0700

    ddd

--- my-app/pages/post/[id].js
+++ my-app/pages/post/[id].js
@@ -1,10 +1,19 @@
 import Button from "react-bootstrap/Button";
 import React from "react";
-import { Card } from "react-bootstrap";
+import { Card, Spinner } from "react-bootstrap";
 import Layout from "../../components/Layout";
 import { getPostIds, getPostById } from "../../lib/post";
-
+import { useRouter } from "next/dist/client/router";
 const Post = ({ post }) => {
+  const router = useRouter();
+  if (router.isFallback){
+    return(
+      <Spinner animation="border" role='status' variant='dark'>
+        <span className='sr-only'>LOADING </span>
+      </Spinner>
+    )
+  }
+
   return (
     <Layout>
       <Card className="my-3 shadow">
@@ -19,11 +28,11 @@ const Post = ({ post }) => {
 };
 //Lay du lieu kieu tinh, nhung du lieu tinh nao thi con phu thuoc vao params
 export const getStaticPaths = async () => {
-  const paths = await getPostIds();
+  const paths = await getPostIds(5);
   console.log(paths);
   return {
     paths,
-    fallback: false,
+    fallback: true,
   };
 };
 
@@ -33,6 +42,7 @@ export const getStaticProps = async ({ params }) => {
     props: {
       post,
     },
+   
   };
 };
 
